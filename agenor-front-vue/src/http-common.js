5b690ff160a639780a14dfcd244fd0407c27e0c5
marionstaats <70648703+marionstaats@users.noreply.github.com>
2021-08-08 13:02:06 +0200

    changing baseURL in FE

--- agenor-front-vue/src/http-common.js
+++ agenor-front-vue/src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "http://localhost:8081/api",
+    baseURL: "https://agenorbe.herokuapp.com/", //"http://localhost:8081/api",
     headers: {
         "Content-type": "application/json",
     }
