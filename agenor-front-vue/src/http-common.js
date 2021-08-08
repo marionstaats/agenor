cc7c84f69c0a0f4e72784a09be99035bfd222fb3
marionstaats <70648703+marionstaats@users.noreply.github.com>
2021-08-08 13:15:34 +0200

    new dist files/new baseurl

--- agenor-front-vue/src/http-common.js
+++ agenor-front-vue/src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "https://agenorbe.herokuapp.com/", //"http://localhost:8081/api",
+    baseURL: "https://agenorbe.herokuapp.com/api", //"http://localhost:8081/api",
     headers: {
         "Content-type": "application/json",
     }
